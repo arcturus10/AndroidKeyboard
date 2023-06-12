4961e3271b4d20ed75ed7580c9528f552d248e6b
arcturus10 <136338872+arcturus10@users.noreply.github.com>
2023-06-12 20:08:51 +0530

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,46 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Android Keyboard Installer'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(75,120)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(150,120)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
+$description1 = New-Object System.Windows.Forms.Label
+$description1.Location = New-Object System.Drawing.Point(10,20)
+$description1.Size = New-Object System.Drawing.Size(280,20)
+$description1.Text = 'Please enter the location of the ZIP File'
+$form.Controls.Add($description1)
+
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(10,40)
+$textBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBox)
+
+$form.Topmost = $true
+
+$form.Add_Shown({$textBox.Select()})
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $location = $textBox.Text
+}
+Expand-Archive -LiteralPath $location -DestinationPath "\Program Files (x86)\Android Keyboard\application" -cf
+Write-Host "To Uninstall, delete the contents of   \Program Files (x86)\Android Keyboard\v0.1    " --ForegroundColor DarkGreen -BackgroundColor White
